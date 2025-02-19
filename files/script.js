bfd03d0ba65f67e02c53f73340448e8d59ef05dc
kemboi <kemboicosmas4160@gmail.com>
2025-02-19 19:44:41 +0300

    added new files

--- files/script.js
+++ files/script.js
@@ -1,6 +1,6 @@
 
 var typed = new Typed(".text", {
-    strings: ["Full-stack web development" , "Graphic designing" , "Network engineering"],
+    strings: ["Full-stack web development", "Artificial Intelligence", "Cloud computing", "Network engineering"],
     typeSpeed:100,
     backSpeed:50,
     backDelay:1000,
